1d1953929fa32b6fa8ac6995f1ddd6df2aaa1719
N.Glori <nglori1@students.towson.edu>
2026-05-12 12:20:45 -0400

    implemented add to order

--- src/main/java/towsongroup/fooddoods/CustomerController.java
+++ src/main/java/towsongroup/fooddoods/CustomerController.java
@@ -90,6 +90,7 @@ public class CustomerController {
     @FXML
     private void addToOrder() {
         try {
+            // Insert order item into order item table
             PreparedStatement ps = conn.prepareStatement("INSERT INTO Order_Item (OrderItem_ID, Quantity, Price)\n" +
                     "VALUES (?, ?, ?);");
 
@@ -99,7 +100,65 @@ public class CustomerController {
             menuItemIDResult.next();
             String menuItemID = menuItemIDResult.getString(1);
 
+            // Generate ID and check availability
+            int orderItemID = (int) (Math.random() * 1000000);
+            ArrayList<Integer> usedIDs = State.getIDs();
 
+            while (true) {
+                if (usedIDs.contains(orderItemID)) {
+                    orderItemID = (int) (Math.random() * 1000000);
+                } else {
+                    break;
+                }
+            }
+
+            ps.setString(1, Integer.toString(orderItemID));
+            ps.setString(2, "1");
+
+            PreparedStatement getItemPrice = conn.prepareStatement("SELECT Price FROM Menu_Item WHERE MenuItem_ID = /");
+            getItemPrice.setString(1, menuItemID);
+            ResultSet menuItemRS = getItemPrice.executeQuery();
+            menuItemRS.next();
+            String itemPrice = menuItemRS.getString(1);
+
+            ps.setString(3, itemPrice);
+
+            ps.executeUpdate();
+
+            // Insert order into order table
+            PreparedStatement addOrder = conn.prepareStatement("INSERT INTO Order (INSERT INTO Order (Order_ID, O_Restaurant_ID, O_Customer_ID, Status)\n" +
+                    "VALUES (?, ?, ?, ?);)");
+
+            // Generate ID and check availability
+            int orderID = (int) (Math.random() * 1000000);
+
+            while (true) {
+                if (usedIDs.contains(orderID)) {
+                    orderID = (int) (Math.random() * 1000000);
+                } else {
+                    break;
+                }
+            }
+
+            addOrder.setString(1, Integer.toString(orderID));
+
+            PreparedStatement getRestID = conn.prepareStatement("SELECT Restaurant_ID FROM Restaurant WHERE RestaurantName = ?");
+            getRestID.setString(1, restaurantListView.getSelectionModel().getSelectedItem());
+            ResultSet restIDResult = getRestID.executeQuery();
+            restIDResult.next();
+            String restID = restIDResult.getString(1);
+
+            addOrder.setString(2, restID);
+
+            PreparedStatement getCustID = conn.prepareStatement("SELECT C_ID FROM Customer WHERE C_Name = ?");
+            getCustID.setString(1, State.name);
+            ResultSet custIDResult = getCustID.executeQuery();
+            custIDResult.next();
+            String custID = custIDResult.getString(1);
+
+            addOrder.setString(3, custID);
+
+            addOrder.setString(4, "Order sent");
         } catch (Exception e) {
             e.printStackTrace();
         }
