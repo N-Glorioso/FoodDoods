b9fe31df52f882425c9a6f8711a4af48f3f1c7d5
N.Glori <nglori1@students.towson.edu>
2026-05-12 04:35:28 -0400

    Implemented SQL query and code for sign up

--- src/main/java/towsongroup/fooddoods/RestaurantOwnerSignUpController.java
+++ src/main/java/towsongroup/fooddoods/RestaurantOwnerSignUpController.java
@@ -2,11 +2,16 @@ package towsongroup.fooddoods;
 
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Alert;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Pane;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
 
 public class RestaurantOwnerSignUpController {
 
@@ -29,13 +34,108 @@ public class RestaurantOwnerSignUpController {
 
     @FXML
     private void attemptSignIn() {
-        String userName = userNameField.getText();
-        String passWord = passWordField.getText();
-        String name = nameField.getText();
-        String phoneNumber = phoneNumberField.getText();
-        String eMailAddress = eMailAddressField.getText();
-        String restaurantName = restaurantNameField.getText();
-        String restaurantAddress = restaurantAddressField.getText();
+        try {
+            // Set up connection
+            Connection conn = State.getConn();
+
+            // Set up owner account
+            PreparedStatement ownerStatement = conn.prepareStatement("INSERT INTO Restaurant_Owner (Owner_ID, OwnerName, RO_PhoneNum, Email, RO_Username, RO_Password)\n" +
+                    "VALUES (?, ?, ?, ?, ?, ?);");
+
+            String userName = userNameField.getText();
+            String passWord = passWordField.getText();
+            String name = nameField.getText();
+            String phoneNumber = phoneNumberField.getText();
+            String eMailAddress = eMailAddressField.getText();
+            String restaurantName = restaurantNameField.getText();
+            String restaurantAddress = restaurantAddressField.getText();
+
+            // Generate ID and check availability
+            int randID = (int) (Math.random() * 1000000);
+            PreparedStatement getIDs = conn.prepareStatement("SELECT Worker_ID FROM Restaurant_Worker");
+            ResultSet ids = getIDs.executeQuery();
+            outerloop: while (true) {
+                randID = (int) (Math.random() * 1000000);
+                while (ids.next()) {
+                    if (randID == ids.getInt(1)) {
+                        continue outerloop;
+                    }
+                }
+                break;
+            }
+            // Generate Owner ID and check availability
+            int ownerID = (int) (Math.random() * 1000000);
+            ArrayList<Integer> usedIDs = State.getIDs();
+
+            while (true) {
+                if (usedIDs.contains(ownerID)) {
+                    ownerID = (int) (Math.random() * 1000000);
+                } else {
+                    break;
+                }
+            }
+
+            ownerStatement.setString(1, Integer.toString(ownerID));
+
+            ownerStatement.setString(2, name);
+            State.name = name;
+            ownerStatement.setString(3, phoneNumber);
+            State.phoneNumber = phoneNumber;
+            ownerStatement.setString(4, eMailAddress);
+            State.eMailAddress = eMailAddress;
+            ownerStatement.setString(5, userName);
+            State.userName = userName;
+            ownerStatement.setString(6, passWord);
+            State.passWord = passWord;
+
+            // Set up restaurant entry
+            PreparedStatement restaurantStatement = conn.prepareStatement("INSERT INTO Restaurant (Restaurant_ID, R_Owner_ID, Address, RestaurantName)\n" +
+                    "VALUES (?, ?, ?, ?);");
+
+            // Generate Restaurant ID and check availability
+            int restaurantID = (int) (Math.random() * 1000000);
+
+            while (true) {
+                if (usedIDs.contains(restaurantID)) {
+                    restaurantID = (int) (Math.random() * 1000000);
+                } else {
+                    break;
+                }
+            }
+
+            restaurantStatement.setString(1, Integer.toString(restaurantID));
+            restaurantStatement.setString(2, Integer.toString(ownerID));
+            restaurantStatement.setString(3, restaurantAddress);
+            restaurantStatement.setString(4, restaurantName);
+
+            conn.setAutoCommit(false);
+            ownerStatement.executeUpdate();
+            restaurantStatement.executeUpdate();
+
+            PreparedStatement assignRestID = conn.prepareStatement("UPDATE Restaurant_Owner\n" +
+                    "SET RO_Restaurant_ID = ?\n" +
+                    "WHERE Owner_ID = ?");
+            assignRestID.setString(1, Integer.toString(restaurantID));
+            assignRestID.setString(2, Integer.toString(ownerID));
+
+            assignRestID.executeUpdate();
+
+            try {
+                conn.commit();
+            } catch (Exception e) {
+                conn.rollback();
+                throw new Exception("Error");
+            }
+
+            Alert successsAlert = new Alert(Alert.AlertType.INFORMATION);
+            successsAlert.setContentText("Account creation successful");
+            successsAlert.show();
+        } catch (Exception e) {
+            Alert exceptionAlert = new Alert(Alert.AlertType.ERROR);
+            exceptionAlert.setContentText(e.getMessage());
+            exceptionAlert.show();
+            return;
+        }
 
         FXMLLoader loader = new FXMLLoader(App.class.getResource("restaurantOwnerScene.fxml"));
         try {
